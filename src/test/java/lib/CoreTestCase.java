6ed8f56e6942ed23a28bc1e920703cafd63de597
vitaliy kotov <v.kotov@corp.badoo.com>
2021-10-07 18:36:15 +0300

    Webinar 2

--- src/test/java/lib/CoreTestCase.java
+++ src/test/java/lib/CoreTestCase.java
@@ -1,26 +1,39 @@
 package lib;
 
+import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
+import lib.ui.PlatformSelector;
+import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
-
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class CoreTestCase {
 
-    protected AndroidDriver<WebElement> driver;
+    protected AppiumDriver<WebElement> driver;
     private static final String APPIUM_URL = "http://0.0.0.0:4723/wd/hub";
 
     @Before
-    public void setUp() throws MalformedURLException {
+    public void setUp() throws Exception {
         URL URL = new URL(APPIUM_URL);
-        this.driver = new AndroidDriver<>(URL, this.getAndroidDesiredCapabilities());
+
+        if (PlatformSelector.PLATFORM == "ios") {
+            this.driver = new IOSDriver<WebElement>(URL, this.getIOSDesiredCapabilities());
+        } else if (PlatformSelector.PLATFORM == "android") {
+            this.driver = new AndroidDriver<WebElement>(URL, this.getAndroidDesiredCapabilities());
+        } else {
+            throw new Exception("Cannot run Appium session with the platform equals " + PlatformSelector.PLATFORM);
+        }
     }
 
-    private DesiredCapabilities getAndroidDesiredCapabilities()
-    {
+    @After
+    public void tearDown() {
+        this.driver.quit();
+    }
+
+    private DesiredCapabilities getAndroidDesiredCapabilities() {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("platformName","Android");
         capabilities.setCapability("avd","ver10");
@@ -32,4 +45,14 @@ public class CoreTestCase {
         capabilities.setCapability("app","/Users/vitalijkotov/SFMobile/apps/org.wikipedia.apk");
         return capabilities;
     }
+
+    private DesiredCapabilities getIOSDesiredCapabilities() {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "iOS");
+        capabilities.setCapability("deviceName", "iPhone 12 Pro");
+        capabilities.setCapability("platformVersion", "14.2");
+        capabilities.setCapability("app", "/Users/vitalijkotov/SFMobile/apps/Wikipedia.app");
+        return capabilities;
+    }
+
 }
--- src/test/java/tests/SearchTest.java
+++ src/test/java/tests/SearchTest.java
@@ -1,34 +1,25 @@
 package tests;
 
 import lib.CoreTestCase;
-import lib.ui.MainPageObject;
+import lib.ui.pages.factory.OnboardingPageFactory;
+import lib.ui.pages.factory.SearchPageFactory;
+import lib.ui.pages.factory.StartPageFactory;
+import lib.ui.pages.interfaces.IOnboardingPageObject;
+import lib.ui.pages.interfaces.ISearchPageObject;
+import lib.ui.pages.interfaces.IStartPageObject;
 import org.junit.Test;
-import org.openqa.selenium.WebElement;
 
 public class SearchTest extends CoreTestCase {
 
     @Test
-    public void testFindJava() {
-        MainPageObject mainPO = new MainPageObject(this.driver);
+    public void testSearch() {
+        IOnboardingPageObject onboardingPage = OnboardingPageFactory.get(this.driver);
+        IStartPageObject startPage = StartPageFactory.get(this.driver);
+        ISearchPageObject searchPage = SearchPageFactory.get(this.driver);
 
-        WebElement searchInit = mainPO.waitForElementPresent(
-                "id:org.wikipedia:id/search_container",
-                "Cannot find Search Wikipedia init search input"
-        );
-        searchInit.click();
-
-        WebElement searchInput = mainPO.waitForElementPresent(
-                "id:org.wikipedia:id/search_src_text",
-                "Cannot find search input"
-        );
-
-        searchInput.sendKeys("Java");
-
-        WebElement expectedResult = mainPO.waitForElementPresent(
-                "xpath://*[./*[contains(@text,'Island of Indonesia')]]",
-                "Cannot find result 'Island of Indonesia' on 'Java' search"
-        );
-
-        expectedResult.click();
+        onboardingPage.skipOnboarding();
+        startPage.initSearch();
+        searchPage.findByText("Java");
+        searchPage.selectByText("Island of Indonesia");
     }
 }
